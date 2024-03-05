e72905afcdd98ea6d2ae73f886596a9155c0a681
Maria Bursas <maria.bursas@gmail.com>
2024-03-05 15:13:16 +0200

    init commit

---
+++ main.js
@@ -0,0 +1,16 @@
+var personMaria = {
+  firstName: "Maria",
+  lastName: "Bursas",
+};
+var personAna = {
+  firstName: "Ana",
+  lastName: "Pop",
+};
+var personMihai = {
+  firstName: "Mihai",
+  lastName: "Ionescu",
+};
+
+var persons = [personMaria, personAna, personMihai];
+persons[1].age = 25;
+console.log(persons[1]);
